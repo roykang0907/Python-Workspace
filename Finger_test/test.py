437f1e9b78500e39ce799a28d30bb8871fd003ab
roykang0907 <junyeonggang22@gmail.com>
2026-05-03 23:08:38 +0900

    reset

--- Finger_test/test.py
+++
@@ -1,8 +0,0 @@
-import requests
-
-TOKEN = "8742370545:AAEXenn6jmiZXU5IPdrZEh4I1-92b4UyZ7A"
-
-url = f"https://api.telegram.org/bot{TOKEN}/getUpdates"
-response = requests.get(url)
-
-print(response.json())
--- ncnn conversion.py
+++
@@ -1,5 +0,0 @@
-from ultralytics import YOLO
-
-model = YOLO("best.pt")  # Load the YOLO model
-
-model.export(format="ncnn")
